ede51571095372c8b4204d3b32b9602d88f01579
sudeep bachewar <sudeepbachewar96@gmail.com>
2026-04-25 16:34:06 +0530

    Streamlit code

---
+++ gauri.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Love App", layout="centered")
+
+st.markdown("<h1 style='text-align: center;'>❤️ Love App ❤️</h1>", unsafe_allow_html=True)
+
+if st.button("I LOVE YOU"):
+    st.markdown(
+        "<h2 style='text-align: center; color: red;'>fuck of gauri</h2>",
+        unsafe_allow_html=True
+    )
